35cf1befc8f1ff27e5143a13d4d019d22f4bec2a
Adam Hassan <hassanad94@gmail.com>
2019-02-01 01:11:28 +0100

    update primes array functions

--- prime.php
+++ prime.php
@@ -3,13 +3,13 @@ $primes = array();
 
 	function is_prime($number){
 		$divider = 0;
-		for($i=1;$i<=$number/2;$i++)
+		for($i=1;$i<=$number;$i++)
 		{
 			if($number % $i ==0 )
 			{
 				$divider++;
 			}
-			if($divider>2)
+			if($divider>2 || $number/2 == $i)
 			{
 				return false;
 			}
@@ -17,6 +17,20 @@ $primes = array();
 		return true;
 	}
 	function update_primes_array(){
-		
+		$i=1;
+		$match=0;
+		$counter = 0;
+		do
+		{
+			$i++;
+			if(is_prime($i)== true)
+			{
+				$primes[] = $i;
+				$match++;
+			}
+			
+		}while($match != 1000);
 	}
+
+	
 ?>
